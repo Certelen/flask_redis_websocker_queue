bd045166305f0b903c8e93a75fff96401cb20003
Certelen <dst98@yandex.ru>
2024-05-31 12:57:16 +0500

    Рефакторинг

--- run_huey.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Ensure that the huey package is on the python-path, in the event it hasn't
-# been installed using pip.
-export PYTHONPATH="../../:$PYTHONPATH"
-
-# Run the consumer with 2 worker threads.
-python ./backend/tasks/huey_consumer.py main.huey
